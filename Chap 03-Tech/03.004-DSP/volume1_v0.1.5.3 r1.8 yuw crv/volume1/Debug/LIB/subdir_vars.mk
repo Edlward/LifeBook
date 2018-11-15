8b08fe5a1c5e29b424d0a755cef5434c65dd0542
jerhua <jerry.hua@assaabloy.com>
2018-11-15 09:55:25 +0800

    add father path ../../../

--- Chap 03-Tech/03.004-DSP/volume1_v0.1.5.3 r1.8 yuw crv/volume1/Debug/LIB/subdir_vars.mk
+++
@@ -1,9 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
-LIB_SRCS += \
-E:/_workroom/0101-SIMU/0101-SIMU/volume1_v0.1.5.3\ r1.7\ yuww/LIB/rts2800_ml.lib 
-
-
